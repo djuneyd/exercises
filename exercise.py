d2874b3f5ece607485885fd4737d5eafa3b10b87
djuneyd <djuneyd2009@gmail.com>
2023-08-04 12:54:41 +0400

    VALID SUDOKU BOYYYYYYYYYYYYY

--- exercise.py
+++ exercise.py
@@ -1,11 +1,48 @@
-def anagram(s, t):
-    if len(s) == len(t) and set(s) == set(t):
-        for i in range(len(s)):
-            if s[i] in t:
+board = [["8","3",".",".","7",".",".",".","."]
+        ,["6",".",".","1","9","5",".",".","."]
+        ,[".","9","8",".",".",".",".","6","."]
+        ,["8",".",".",".","6",".",".",".","3"]
+        ,["4",".",".","8",".","3",".",".","1"]
+        ,["7",".",".",".","2",".",".",".","6"]
+        ,[".","6",".",".",".",".","2","8","."]
+        ,[".",".",".","4","1","9",".",".","5"]
+        ,[".",".",".",".","8",".",".","7","9"]]
+
+string = 0
+num = 0
+numvalue = 0
+answer = True
+pre = []
+squares = []
+columns = []
+
+
+#division into squares
+for k in range(3):
+        for d in range(3):
+                for i in range(3):
+                        for j in range(3):
+                                pre.append(board[string][num])
+                                num += 1
+                        string += 1
+                        num = numvalue
+                squares.append(pre)
+                pre = []
+        string = 0
+        numvalue += 3
+        num = numvalue
+
+#division into columns
+for i in range(9):
+        for j in range(9):
+                pre.append(board[j][i])
+        columns.append(pre)
+        pre = []
+
+#logic
+for i in range(9):
+        if len(set(board[i])) == (len(board[i]) - board[i].count(".") + 1) and len(set(squares[i])) == (len(squares[i]) - squares[i].count(".") + 1) and len(set(columns[i])) == (len(columns[i]) - columns[i].count(".") + 1):
                 continue
-            else:
-                return False
-        return True
-    else:
-        return False
-print(anagram('anagram', 'magaran'))
+        else:
+                answer = False
+                break
